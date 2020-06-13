ec7871892fb09388022df8647c82ad7a2543db78
AhmedAlhejaili <ahmedalhejaili@gmail.com>
2020-06-13 03:54:18 +0300

    Create operators.py
    True False

---
+++ operators.py
@@ -0,0 +1,30 @@
+#num1 = 5
+#num2 = 6
+#print(num1 < num2)
+#print(5 == 6)
+#print(5 != 6)
+#print(6 >= 7)
+#print(6 >= 6)
+#print( 5 <= 6)
+
+# True , False , and , or , not
+#print( True and True) # True
+#print(True and False) # False
+#print( False and False) # False
+a,b = 1,2
+print(a == a and b == b)
+print(a == b and b == b)
+print( a == b and b == a)
+
+#print(True or True)# True
+#print(True or False)# True
+#print(False or False)# False
+print(a < a or b == b)
+print(a > b or b == b)
+print( a == b or b < a)
+
+#print(not True) # False
+#print(not False) # True
+print(not (a !=5 ))
+print(not (b == 9))
+
