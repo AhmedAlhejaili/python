b26de44b6bc4efa9edcecadfa74b57c2828dfab5
AhmedAlhejaili <ahmedalhejaili@gmail.com>
2020-06-16 19:55:20 +0300

    def

---
+++ functions.py
@@ -0,0 +1,20 @@
+def liner():
+    print('----------------------')
+
+def addtion(num1,num2):
+    print(int (num1) + int (num2))
+    print(int (num1) - int (num2))
+    print(int (num1) * int (num2))
+    print(int (num1) / int (num2))
+
+n1 = input('Please inter the first number:')
+n2 = input('Please unter the scond number:')
+addtion(n1,n2)
+
+#print('ahmed muhanna')
+#liner()
+#print(28)
+#liner()
+
+
+
