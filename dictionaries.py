a5c26735039f3a699b6a90e416d0baa72d0208bd
AhmedAlhejaili <ahmedalhejaili@gmail.com>
2020-06-16 19:24:05 +0300

    dictionaris

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+subjects_whit_no_student = {"Arabic":3, "English":3, "Math":2}
+print(subjects_whit_no_student)
+students = {"Arabic":["Ali ","ahmed","mona"],"English":["jack", "truki", "mohmmad"]}
+print(students)
+students['math'] = ['salah', 'hussen']
+print(students)
+students.update({"history" : "fadi"})
+print(students)
