f638efd50b852fbea1a5b81fd1231b239ef9d942
AhmedAlhejaili <ahmedalhejaili@gmail.com>
2020-06-16 19:04:43 +0300

    forloop

---
+++ forloop.py
@@ -0,0 +1,19 @@
+subjects = ['Arabic', 'English', 'Math']
+print(subjects)
+print(subjects[2])
+print(subjects[0])
+subjects.append('History')
+print(subjects)
+print(subjects[1:3])
+print(subjects[1:4])
+print(subjects[-1]) # اخر عنصر في الريهs
+# subjects.pop()
+# print(subjects)
+# subjects.pop(0)
+# print(subjects)
+
+
+for x in subjects:
+    print(x)
+
+
