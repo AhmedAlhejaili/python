8f18b8e8d065f53cc6f670a757d4146244189229
AhmedAlhejaili <ahmedalhejaili@gmail.com>
2020-06-16 18:50:43 +0300

    whileloop

--- whileloop.py
+++ whileloop.py
@@ -1,13 +1,13 @@
-num_of_tries=0
-
-while num_of_tries !=3:
-username = input( 'Please insert your username:')
-password = input('Please insert your password:')
-
-if username == 'ahmed' and password == '123':
-    print(welcome)
-else:
-    print('error')
-    num_of_tries +=1
-    print('you have'+str(3 - num_of_trise) + " left")
-
+num_of_tries = 0
+while num_of_tries != 3:
+    username = input('Please insert your username:')
+    password = input('Please insert your password:')
+    if username == 'ahmed' and password == '123':
+        print('welcome')
+        num_of_tries = 0
+        exit()
+    else:
+        print('error')
+        num_of_tries += 1
+        print('you have' + str(3 - num_of_tries) + "tries left")
+         
